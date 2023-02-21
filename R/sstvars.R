ea6be4fe75c18417080afeb11aa862dad192b273
saviviro <43138478+saviviro@users.noreply.github.com>
2023-02-21 12:06:53 +0200

    Rcpp init + sstvars doc

---
+++ R/sstvars.R
@@ -0,0 +1,17 @@
+#' @title sstvars: toolkit for structural smooth transition vector autoregressive models
+#'
+#' @description \code{sstvars} is a package for reduced form and structural smooth transition vector
+#'   autoregressive models with focus on the structural analysis. It provides functions for ...
+#'
+#'   Some of the code is taken from the R package gmvarkit (also created by the author of this package)
+#'   but is copied rather than using the code in gmvarkit directly in order to allow independent development.
+#'
+#'   The readme file is a good place to start and the vignette might be useful too.
+#'
+#' @docType package
+#' @author you <savi.virolainen@helsinki.fi>
+#' @import Rcpp parallel pbapply
+#' @importFrom Rcpp evalCpp
+#' @useDynLib sstvars
+#' @name sstvars
+NULL
