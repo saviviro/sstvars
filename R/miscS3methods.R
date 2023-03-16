8a8dce60082a87b7f79f860913aa1b8e830e9250
saviviro <43138478+saviviro@users.noreply.github.com>
2023-03-16 12:38:09 +0200

    summary.stvar

--- R/miscS3methods.R
+++ R/miscS3methods.R
@@ -15,16 +15,23 @@ residuals.stvar <- function(object, ...) {
 }
 
 
-# #' @describeIn STVAR summary method
-# #' @inheritParams logLik.stvar
-# #' @inheritParams print.stvar
-# #' @export
-# summary.stvar <- function(object, ..., digits=2) {
-#   stvar <- object
-#   structure(list(gsmvar=gsmvar,
-#                  abs_boldA_eigens=get_boldA_eigens(stvar),
-#                  omega_eigens=get_omega_eigens(stvar),
-#                  regime_means=get_regime_means(stvar),
-#                  digits=digits),
-#             class="stvarsum")
-# }
+#' @describeIn STVAR summary method
+#' @inheritParams logLik.stvar
+#' @inheritParams print.stvar
+#' @export
+summary.stvar <- function(object, ..., digits=2) {
+  stvar <- object
+  structure(list(stvar=stvar,
+                 abs_boldA_eigens=get_boldA_eigens(stvar),
+                 omega_eigens=get_omega_eigens(stvar),
+                 regime_means=get_regime_means(p=stvar$model$p, M=stvar$model$M, d=stvar$model$d,
+                                               weight_function=stvar$model$weight_function,
+                                               cond_dist=stvar$model$cond_dist,
+                                               parametrization=stvar$model$parametrization,
+                                               identification=stvar$model$identification,
+                                               AR_constraints=stvar$model$AR_constraints,
+                                               mean_constraints=stvar$model$mean_constraints,
+                                               B_constraints=stvar$model$B_constraints),
+                 digits=digits),
+            class="stvarsum")
+}
