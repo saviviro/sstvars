7622afa1f18618958d5ca8b5efd89a48bb708cce
saviviro <43138478+saviviro@users.noreply.github.com>
2023-04-24 14:23:25 +0300

    d_lift completed

--- R/jointSpectralRadius.R
+++ R/jointSpectralRadius.R
@@ -45,52 +45,31 @@ d_lift <- function(A, d) {
   }
   all_mu_multisets <- apply(all_multisets, MARGIN=1, FUN=get_mu_multiset) # mu(multiset) calculated for all multisets
 
-  A_lifted <- matrix(nrow=N, ncol=N)
+  A_lifted <- matrix(nrow=N, ncol=N) # Initialize A_lifted
 
-  get_elements <- Vectorize(function(i, j) { # Used in obtaining subsets A(alpha, beta)
-    A[alpha[j], beta[i]]
+  get_elements <- Vectorize(function(i, j) { # Used in obtaining "submatrices" A(alpha, beta) with alpha and beta multisets
+    A[alpha[j], beta[i]] # Notice the indexing (j and i "swapped")
   })
 
+  # Calculate the elements of A_lifted using Equation (8) of Parrilo and Jadbabaie (2008)
   for(i1 in 1:N) { # i1 = row index
     for(i2 in 1:N) { # i2 = column index
-       alpha <- all_multisets[i1,]
-       beta <- all_multisets[i2,]
-       A_submat <- outer(seq_len(d), seq_len(d), get_elements)
-       print(A_submat)
+       alpha <- all_multisets[i1,] # get_element uses this
+       beta <- all_multisets[i2,] # get_elements uses this
+       A_submat <- outer(seq_len(d), seq_len(d), get_elements) # "Submatrix" A(alpha,beta)
+       A_lifted[i1, i2] <-  get_permanent_Cpp(A_submat)/sqrt(all_mu_multisets[i1]*all_mu_multisets[i2]) # Equation (8)
     }
   }
+
+  # # Below works also for creating the "submatrix" A_submat but is slow as uses loops
+  # A_submat <- matrix(0, nrow = length(alpha), ncol = length(beta))
+  # # Loop through the elements of a and b and populate matrix B
+  # for (i in 1:length(alpha)) {
+  #   for (j in 1:length(beta)) {
+  #     A_submat[i, j] <- A[alpha[j], beta[i]]
+  #   }
+  # }
+
+  A_lifted
 }
-
-A <- matrix(c(11, 21, 31, 12, 22, 32, 13, 23, 33), nrow=3)
-A <- matrix(c(0.5, 0.2, -0.1, 0.6), nrow=2)
-A <- matrix(c(11, 21, 12, 22), nrow=2)
-
-A <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
-
-a <- c(1, 1, 2)
-b <- c(1, 2, 2)
-
-A <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
-a <- c(1, 3)
-b <- c(2, 3)
-# Initialize B with the same dimensions as the length of a and b
-
-
-
-# Create a function to extract elements from A using a and b
-get_elements <- Vectorize(function(i, j) {
-  A[a[j], b[i]]
-})
-
-# Create matrix B using outer() function and the get_elements() function
-B <- outer(1:length(a), 1:length(b), Vectorize(get_elements))
-
-# # Below works for creating the submatrix A_submat but is slow
-# A_submat <- matrix(0, nrow = length(alpha), ncol = length(beta))
-# # Loop through the elements of a and b and populate matrix B
-# for (i in 1:length(alpha)) {
-#   for (j in 1:length(beta)) {
-#     A_submat[i, j] <- A[alpha[j], beta[i]]
-#   }
-# }
 
