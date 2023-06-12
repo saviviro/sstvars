d21e20e2eb76f52285cf383d253c653a30dfa98a
saviviro <43138478+saviviro@users.noreply.github.com>
2023-06-12 16:32:03 +0300

    diagnostic plot to student

--- R/diagnosticPlot.R
+++ R/diagnosticPlot.R
@@ -19,6 +19,8 @@
 #' @param maxlag the maximum lag considered in types \code{"ac"} and \code{"ch"}.
 #' @details Auto- and cross-correlations (types \code{"ac"} and \code{"ch"}) are calculated with the function
 #'  \code{acf} from the package \code{stats} and the plot method for class \code{'acf'} objects is employed.
+#'  If \code{cond_dist == "Student"}, the estimate of the degrees of freedom parameter is used in theoretical
+#'  densities and quantiles.
 #' @inherit get_residuals references
 #' @seealso \code{\link{profile_logliks}}, \code{\link{fitSTVAR}}, \code{\link{STVAR}},
 #'  \code{\link{LR_test}}, \code{\link{Wald_test}}, \code{\link{cond_moment_plot}}
@@ -90,18 +92,34 @@ diagnostic_plot <- function(stvar, type=c("all", "series", "ac", "ch", "dist"), 
     acf(res^2, lag.max=maxlag, plot=TRUE, ylab="")
   }
   if(type == "dist" || type == "all") {
-    if(stvar$model$cond_dist != "Gaussian") stop("Only Gaussian cond_dist is currently implemented to diagnostic_plot")
-    # KUN TULEE STUDENT NIIN MUISTA LAITTAA TS JA DIST PLOTTEIHIN TIETO JAKAUMASTA JOHONKIN
     par(mfrow=c(2, d), mar=c(2.5, 2.8, 2.1, 1.0))
+    if(stvar$model$cond_dist == "Gaussian") {
+      dens_fun <- function(y) dnorm(y)
+      qqplot_fun <- function(y) qqnorm(y, main="", ylab="", xlab="")
+      qqline_fun <- function(y) qqline(y, col="darkred")
+    } else { # cond_dist == "Student"
+      df <- stvar$params[length(stvar$params)] # The last param is always the df param
+      dens_fun <- function(y) dt(y, df=df)
+      qqplot_fun <- function(y){
+        y <- sort(y, decreasing=FALSE) # Sorted sample quantiles
+        T_obs <- length(y)
+        p <- (1:T_obs - 0.5)/T_obs  # Probs; 0.5 substracted to avoid 0 and 1 that would result in -Inf and Inf
+        t_quantiles <- qt(p, df=df)  # Theoretical quantiles; df taken from parent env
+        plot(x=t_quantiles, y=y, main="", xlab="", ylab="") # Plot samples quantes agains theoretical quants
+      }
+      qqline_fun <- function(y) qqline(y, col="darkred", distribution=function(p) qt(p, df=df))
+    }
+    # Plot histograms with theoretical density
     for(i1 in 1:d) {
       hs <- hist(res[,i1], breaks="Scott", probability=TRUE, col="skyblue", plot=TRUE,
-                 main=colnames(res)[i1], ylim=c(0, 0.5))
+                 main=colnames(res)[i1], ylim=c(0, 0.5), ylab="", xlab="")
       x <- seq(from=min(hs$breaks), to=max(hs$breaks), length.out=1000)
-      lines(x=x, y=dnorm(x), lty=2, col="darkred")
+      lines(x=x, y=dens_fun(x), lty=2, col="darkred", lwd=2)
     }
+    # Plot QQ plots with theoretical quantiles
     for(i1 in 1:d) {
-      qqnorm(res[,i1], main="")
-      qqline(res[,i1], col="darkred")
+      qqplot_fun(res[,i1])
+      qqline_fun(res[,i1])
     }
   }
 }
