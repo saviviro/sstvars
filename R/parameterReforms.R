965181c5d7b3e216a74962b06bc9a34b4d0a76ec
saviviro <43138478+saviviro@users.noreply.github.com>
2024-04-27 17:04:11 +0300

    typo fix

--- R/parameterReforms.R
+++ R/parameterReforms.R
@@ -5,7 +5,7 @@
 #'
 #' @inheritParams loglikelihood
 #' @return Returns the data reformed into a \eqn{((n_{obs}-p+1)x(dp))} matrix. The i:th row
-#'   of the matrix contains the vector \eqn{(y_{i-1}',...,y_{i-p}')} \eqn{((dp)x1)}, where
+#'   of the matrix contains the vector \eqn{(y_{i-1},...,y_{i-p})} \eqn{((dp)x1)}, where
 #'   \eqn{y_{i}=(y_{1i},...,y_{di})} \eqn{(dx1)}.
 #' @section Warning:
 #'  No argument checks!
